b5b5d29fae1d9b728d10d1fbd7cc51f7c44c8be1
Ludvinae <benoit.champlon79@gmail.com>
2025-12-17 21:25:58 +0100

    feat(Player): added basic Player logic

---
+++ src/Player.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Player {
+
+    ArrayList<Card> hand;
+    short score;
+
+    public Player() {
+        this.score = 0;
+        this.hand = new ArrayList<Card>();
+    }
+
+    public void setScore() {
+        short tempScore = 0;
+        for (Card card : hand) {
+            tempScore += card.getValue();
+        }
+        this.score = tempScore;
+    }
+
+
+}
