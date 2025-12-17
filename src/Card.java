ba574d6caebeca194ae0c192184369fb88fa3e10
Ludvinae <benoit.champlon79@gmail.com>
2025-12-17 18:44:33 +0100

    initial commit

---
+++ src/Card.java
@@ -0,0 +1,52 @@
+import java.util.List;
+
+public class Card {
+    String card;
+    String type;
+    short value;
+
+    List<String> validCard = List.of("Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K");
+    List<String> validType = List.of("Diamonds", "Hearts", "Spades", "Clubs");
+
+    public Card(String card, String type) throws Exception {
+        setValue(card);
+        setType(type);
+        this.value = getValue();
+    }
+
+    @Override
+    public String toString() {
+        return card + " of " + type;
+    }
+
+    public void setValue(String value) throws Exception {
+        if (validCard.contains(value)) {
+            this.card = value;
+        }
+        else {
+            throw new Exception("Invalid card value");
+        }
+    }
+
+    public void setType(String type) throws Exception {
+        if (validType.contains(type)) {
+            this.type = type;
+        }
+        else {
+            throw new Exception("Invalid card type");
+        }
+    }
+
+    public short getValue() {
+        if (this.card == "J" || this.card == "Q" || this.card == "K") {
+            return 10;
+        } else if (this.card == "Ace") {
+            return 11;
+        }
+        else {
+            return Short.parseShort(this.card);
+        }
+    }
+
+
+}
