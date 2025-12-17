6c675903c19beadf01dc955dee290c8338d0bf2b
Ludvinae <benoit.champlon79@gmail.com>
2025-12-17 20:26:46 +0100

    feat(Deck): initial logic for Deck class

---
+++ src/Deck.java
@@ -0,0 +1,18 @@
+
+import java.util.List;
+
+public class Deck {
+
+    List<Card> startingDeck;
+    List<Card> remainingCards;
+
+    public Deck() {
+        this.remainingCards = getStartingDeck();
+    }
+
+    public List<Card> getStartingDeck() {
+
+    }
+
+
+}
