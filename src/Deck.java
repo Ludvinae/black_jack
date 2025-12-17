2bde75787279b145ad57ff650e45356cd74aa9e6
Ludvinae <benoit.champlon79@gmail.com>
2025-12-17 21:04:20 +0100

    feat(Deck): added method to draw a card from a deck

--- src/Deck.java
+++ src/Deck.java
@@ -1,19 +1,37 @@
 
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 public class Deck {
 
-
+    ArrayList<Card> startingDeck = getStartingDeck();
     ArrayList<Card> remainingCards;
 
     public Deck() throws Exception {
-        this.remainingCards = getStartingDeck();
+        this.remainingCards = startingDeck;
     }
 
     public ArrayList<Card> getStartingDeck() throws Exception {
         return Card.generateDeck();
     }
 
+    @Override
+    public String toString() {
+        return "A deck of " + startingDeck.size() + " cards with " + remainingCards.size() + " cards remaining.";
+    }
 
+
+    public Card drawCard() {
+        Card card;
+        if (!remainingCards.isEmpty()) {
+            int randomIndex = ThreadLocalRandom.current().nextInt(remainingCards.size());
+            card = remainingCards.get(randomIndex);
+            remainingCards.remove(card);
+        }
+        else {
+            card = null;
+        }
+        return card;
+    }
 }
