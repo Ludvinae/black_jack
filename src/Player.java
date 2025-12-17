2cc55b9c9a0bae36c7b74f9b1f33a2dc6a4ac62f
Ludvinae <benoit.champlon79@gmail.com>
2025-12-17 22:38:51 +0100

    feat(Player): added boolean method to decide if a player keep drawing cards

--- src/Player.java
+++ src/Player.java
@@ -1,13 +1,19 @@
+import java.io.StringBufferInputStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Player {
 
     ArrayList<Card> hand;
     short score;
+    boolean keep;
+
+    Scanner playerChoice = new Scanner(System.in);
 
     public Player() {
         this.score = 0;
         this.hand = new ArrayList<Card>();
+        this.keep = false;
     }
 
     public void setScore() {
@@ -29,6 +35,21 @@ public class Player {
 
     public boolean bust(short score) {
         return score > 21;
+    }
+
+    public boolean keepPlaying(boolean cpu) {
+        if (!cpu) {
+            System.out.println("Do you want to draw a card? (Y/n)");
+            String answer = playerChoice.nextLine();
+            if (answer == "n" || answer == "N") {
+                return false;
+            }
+            return true;
+        }
+        if (getScore() <= 16) {
+            return true;
+        }
+        return false;
     }
 
 }
